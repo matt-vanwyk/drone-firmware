281aadb437088200b5143fcfab2abb0f9487b52d
matt-vanwyk <matt.vanwyk2@gmail.com>
2025-09-05 08:53:54 +0200

    Remove build artifacts and improve gitignore

--- ros2_ws/build/drone_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/spiderweb/autonomous-drone-system/drone-firmware/ros2_ws/install/drone_package'
