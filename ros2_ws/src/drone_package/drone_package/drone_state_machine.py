895d76d4cc67fa600e0d8359920ebef117ab820c
matt-vanwyk <matt.vanwyk2@gmail.com>
2025-09-10 11:07:45 +0200

    Created UploadMission.srv in shared_interfaces package. This service is used to upload a mission to the drone from the base station once the drone has confirmed that it is ready to receive a mission via the PrepareForMission.srv

--- ros2_ws/src/drone_package/drone_package/drone_state_machine.py
+++ ros2_ws/src/drone_package/drone_package/drone_state_machine.py
@@ -3,7 +3,7 @@ import rclpy
 from rclpy.node import Node
 from statemachine import State, StateMachine
 from shared_interfaces.msg import DroneState
-from shared_interfaces.srv import PrepareForMission
+from shared_interfaces.srv import PrepareForMission, UploadMission
 from drone_interfaces.msg import Telemetry
 
 class DroneStateMachine(StateMachine):
@@ -35,15 +35,19 @@ class DroneStateMachineNode(Node):
             self.telemetry_callback,
             10
         )
-
         self.drone_state_publisher = self.create_publisher(DroneState, 'drone/state', 10)
         self.create_timer(1.0, self.publish_drone_state)
 
-        # Service Server Declarations (Base_station_state_machine communication)
+        # Service Server Declarations (base_station_state_machine communication)
         self.prepare_mission_service = self.create_service(
             PrepareForMission,
             'drone/prepare_for_mission',
             self.handle_prepare_for_mission
+        )
+        self.upload_mission_service = self.create_service(
+            UploadMission,
+            'drone/upload_mission',
+            self.handle_upload_mission
         )
 
         # Initialize DroneState attributes
@@ -62,6 +66,10 @@ class DroneStateMachineNode(Node):
         self.drone_state.velocity_z = -500.0
         self.drone_state.current_yaw = -500.0
         self.drone_state.mission_complete = False
+
+        # Mission Variables
+        self.current_mission_id = ""
+        self.current_waypoints = []
 
         # THRESHOLDS TO BE PLACED IN CONFIG FILE LATER?
         self.BATTERY_CHARGED_THRESHOLD = 90.0
@@ -84,6 +92,39 @@ class DroneStateMachineNode(Node):
         }
 
         self.get_logger().info('Drone State Machine started - waiting for telemetry...')
+
+    def telemetry_callback(self, msg):
+        """Process incoming telemetry and update state machine"""
+        # Store latest telemetry
+        self.drone_state = msg
+
+        if self.received_first_telemetry_flag is False:
+            self.get_logger().info('Received first telemetry message! Publishing drone state...')
+            self.received_first_telemetry_flag = True
+
+        self.update_state_machine()
+
+    def publish_drone_state(self):
+        """Publish the current drone state"""
+        msg = DroneState()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.latitude = self.drone_state.latitude
+        msg.longitude = self.drone_state.longitude
+        msg.altitude = self.drone_state.altitude
+        msg.armed = self.drone_state.armed
+        msg.flight_mode = self.drone_state.flight_mode
+        msg.is_in_air = self.drone_state.is_in_air
+        msg.battery_percentage = self.drone_state.battery_percentage
+        msg.num_satellites = self.drone_state.num_satellites
+        msg.landed_state = self.drone_state.landed_state
+        msg.velocity_x = self.drone_state.velocity_x
+        msg.velocity_y = self.drone_state.velocity_y
+        msg.velocity_z = self.drone_state.velocity_z
+        msg.current_yaw = self.drone_state.current_yaw
+        msg.mission_complete = self.drone_state.mission_complete
+        msg.current_state = str(self.state_machine.current_state) if self.state_machine.current_state else "UNKNOWN"
+
+        self.drone_state_publisher.publish(msg)
     
     def check_telemetry_validity(self):
         """Check if received telemetry data is valid"""
@@ -122,17 +163,6 @@ class DroneStateMachineNode(Node):
             
         return self.telemetry_valid
 
-    def telemetry_callback(self, msg):
-        """Process incoming telemetry and update state machine"""
-        # Store latest telemetry
-        self.drone_state = msg
-
-        if self.received_first_telemetry_flag is False:
-            self.get_logger().info('Received first telemetry message! Publishing drone state...')
-            self.received_first_telemetry_flag = True
-
-        self.update_state_machine()
-
     def update_state_machine(self):
         """Handle all state transitions and requests"""
         current_state = self.state_machine.current_state.id
@@ -144,27 +174,9 @@ class DroneStateMachineNode(Node):
             self.get_logger().info(f'Drone state: {current_state}')
             self.prev_state = current_state
 
-    def publish_drone_state(self):
-        """Publish the current drone state"""
-        msg = DroneState()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.latitude = self.drone_state.latitude
-        msg.longitude = self.drone_state.longitude
-        msg.altitude = self.drone_state.altitude
-        msg.armed = self.drone_state.armed
-        msg.flight_mode = self.drone_state.flight_mode
-        msg.is_in_air = self.drone_state.is_in_air
-        msg.battery_percentage = self.drone_state.battery_percentage
-        msg.num_satellites = self.drone_state.num_satellites
-        msg.landed_state = self.drone_state.landed_state
-        msg.velocity_x = self.drone_state.velocity_x
-        msg.velocity_y = self.drone_state.velocity_y
-        msg.velocity_z = self.drone_state.velocity_z
-        msg.current_yaw = self.drone_state.current_yaw
-        msg.mission_complete = self.drone_state.mission_complete
-        msg.current_state = str(self.state_machine.current_state) if self.state_machine.current_state else "UNKNOWN"
-
-        self.drone_state_publisher.publish(msg)
+###################################
+# HANDLERS FOR SERVICE REQUESTS FROM BASE STATION STATE MACHINE
+###################################
 
     def handle_prepare_for_mission(self, request, response):
         """Handle prepare for mission service request with all validation inline"""
@@ -183,7 +195,7 @@ class DroneStateMachineNode(Node):
             response.error_message = "Telemetry not valid yet"
         elif current_state != 'idle':
             response.success = False
-            response.error_message = f"Not in idle state: {current_state}"
+            response.error_message = f"Not in idle state! Current state: {current_state}"
         elif self.drone_state.battery_percentage < self.BATTERY_CHARGED_THRESHOLD:
             response.success = False
             response.error_message = f"Battery too low: {self.drone_state.battery_percentage}%"
@@ -203,6 +215,46 @@ class DroneStateMachineNode(Node):
                 response.error_message = f"Transition failed: {str(e)}"
 
         return response
+
+    def handle_upload_mission(self, request, response):
+        """Handle upload mission service request with all validation inline"""
+        self.get_logger().info(f'Mission upload request: {request.mission_id}')
+
+        current_state = self.state_machine.current_state.id
+
+        # Fill in current status
+        response.drone_state = current_state
+        response.num_waypoints_received = len(request.waypoints)
+
+        # Check if in correct state to fly
+        if current_state != 'ready_to_fly':
+            response.success = False
+            response.error_message = f"Not in ready_to_fly state! Current state: {current_state}"
+        else:
+            # Store mission details
+            try:
+                self.current_mission_id = request.mission_id
+                self.current_waypoints = request.waypoints
+                self.state_machine.received_mission()
+                # TODO After this, we need to try and uplaod the mission to the drone
+                # via the mavsdk_node. To do this we will make a service request using
+                # drone/interfaces/srv/UploadMissionMAVSDK.srv (not implemented yet).
+                # Only when we receive a successful response from the mavsdk_node server
+                # will we respond to the base station state machine to say that the mission
+                # upload was successful.
+                response.success = True
+                response.error_message = ""
+                response.drone_state = str(self.state_machine.current_state.id)
+                self.get_logger().info(f'Mission uploaded: {len(request.waypoints)} waypoints')
+            except Exception as e:
+                response.success = False
+                response.error_message = f"Mission upload failed: {str(e)}"
+
+        return response
+
+###################################
+# HANDLERS FOR SERVICE REQUESTS FROM BASE STATION STATE MACHINE
+###################################
 
 def main():
     rclpy.init()
